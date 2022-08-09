e321780977344b290be61871efbb3538b51a8e10
Aval <aval.es@mail.com>
2022-08-09 16:39:49 -0400

    slider

--- assets/js/fecha.js
+++ assets/js/fecha.js
@@ -36,32 +36,7 @@ var x = setInterval(function() {
   }
 }, 1000);
 
-
-const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
-  Dots: false,
-  on: {
-    createSlide: (carousel, slide) => {
-      slide.Panzoom = new Panzoom(slide.$el.querySelector(".panzoom"), {
-        panOnlyZoomed: true,
-        resizeParent: true,
-      });
-    },
-    deleteSlide: (carousel, slide) => {
-      if (slide.Panzoom) {
-        slide.Panzoom.destroy();
-        slide.Panzoom = null;
-      }
-    },
-  },
-});
-
-const thumbCarousel = new Carousel(document.querySelector("#thumbCarousel"), {
-  Sync: {
-    target: mainCarousel,
-    friction: 0,
-  },
-  Dots: false,
-  Navigation: false,
-  center: true,
-  infinite: false,
-});
+$('.carousel').carousel({
+    interval: 3000,
+    pause: false
+  })
